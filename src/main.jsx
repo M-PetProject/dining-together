52467c58c0bdae5dc6b2d923f33ed16493c7af47
jake <pcx474@gmail.com>
2023-04-13 15:16:36 +0900

    react-query, devtools 적용

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/style.css'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <QueryClientProvider client={queryClient}>
+          {/* devtools */}
+          <ReactQueryDevtools initialIsOpen={true} />
+          <App />
+      </QueryClientProvider>
   </React.StrictMode>,
 )
