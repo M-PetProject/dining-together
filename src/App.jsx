de160ae0882f369acacd7aefb40cf2db92abf801
jake <pcx474@gmail.com>
2023-04-13 09:07:37 +0900

    prettier 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {RecoilRoot} from 'recoil';
-import {Routes, BrowserRouter, Route} from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage.jsx';
 import SecondPage from './pages/SecondPage.jsx';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
     return (
         <RecoilRoot>
             <BrowserRouter>
                 <Routes>
-                    <Route exact path='/' element={<MainPage/>}/>
-                    <Route exact path='/second' element={<SecondPage/>}/>
+                    <Route exact path='/' element={<MainPage />} />
+                    <Route exact path='/second' element={<SecondPage />} />
+                    <Route exact path='/login' element={<LoginPage />} />
                 </Routes>
             </BrowserRouter>
         </RecoilRoot>
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,8 @@
+import { Box, Button, Card, CardActions, CardContent, CardHeader, TextField, Typography } from '@mui/material';
+import React from 'react';
+
+const LoginPage = () => {
+    return <div>dsad</div>;
+};
+
+export default LoginPage;
