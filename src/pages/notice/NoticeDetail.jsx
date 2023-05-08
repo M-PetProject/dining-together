b6654e30ffee9ee7980c3b63b70d48f01f101eb0
jake <pcx474@gmail.com>
2023-05-08 17:06:25 +0900

    공지사항 상세

--- src/pages/notice/NoticeDetail.jsx
+++ src/pages/notice/NoticeDetail.jsx
@@ -3,11 +3,14 @@ import { headerState } from '../../atoms/atom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { useNavigate, useParams } from 'react-router-dom';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { Button, IconButton } from '@mui/material';
+import { Avatar, Button, Card, CardContent, CardHeader, IconButton, Stack, Typography } from '@mui/material';
 import { teamMemberState } from '../../atoms/atom';
 import { useNoticeDetailQuery } from '../../api/useQuerys';
 import { CommentType } from '../../enum/enum';
 import Comment from '../../components/Comment';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { dateFormat } from '../../util/cm_util';
+import MessageIcon from '@mui/icons-material/Message';
 
 const NoticeDetail = () => {
   const { idx: noticeIdx } = useParams();
@@ -17,17 +20,45 @@ const NoticeDetail = () => {
     return 'loading';
   }
 
-  //   console.log(svc.noticeQuery.data);
+  console.log(svc.noticeQuery.data.data);
+  const { title, content, memberVo, noticeDtStart, noticeDtEnd, noticeComments } = svc.noticeQuery.data.data;
+  const { memberId, memberName } = memberVo;
+  const { totalItems } = noticeComments;
   return (
-    <div style={{ overflow: 'auto' }}>
-      NoticeDetail
-      <Comment
-        commentType={CommentType.NOTC}
-        teamIdx={svc.teamInfoState.teamIdx}
-        postIdx={noticeIdx}
-        commentList={svc.noticeQuery.data.data.noticeComments.data}
-      />
-    </div>
+    <Stack spacing={2}>
+      <Card>
+        <CardHeader
+          title={memberName}
+          subheader={`${dateFormat(noticeDtStart)} - ${dateFormat(noticeDtEnd)}`}
+          avatar={<Avatar>{memberName[0]}</Avatar>}
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+        />
+        <CardContent>
+          <Typography variant="body1" color="text.secondary">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {content}
+          </Typography>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+            }}
+          >
+            <MessageIcon />
+            <span>{totalItems}</span>
+          </div>
+          <Typography variant="body2" color="text.secondary" align="right"></Typography>
+        </CardContent>
+      </Card>
+      <Comment commentType={CommentType.NOTC} teamIdx={svc.teamInfoState.teamIdx} postIdx={noticeIdx} />
+    </Stack>
   );
 };
 
