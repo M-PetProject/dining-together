db22b9277fbbc26d36408236a332853341ff9323
jake <pcx474@gmail.com>
2023-05-18 14:52:36 +0900

    mui joy 적용

--- src/App.jsx
+++ src/App.jsx
@@ -17,120 +17,132 @@ import NoticeWrite from './pages/notice/NoticeWrite.jsx';
 import RoundTab from './components/RoundTab.jsx';
 import PlaceWritePage from './pages/place/PlaceWritePage';
 import PlaceDetailPage from './pages/place/PlaceDetailPage';
+import {
+  experimental_extendTheme as materialExtendTheme,
+  Experimental_CssVarsProvider as MaterialCssVarsProvider,
+  THEME_ID as MATERIAL_THEME_ID,
+} from '@mui/material/styles';
+import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy/styles';
+const materialTheme = materialExtendTheme();
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <PrivateRoute>
-                <MainPage />
-              </PrivateRoute>
-            }
-          />
-          <Route exact path="/team">
-            <Route
-              exact
-              path="add"
-              element={
-                <PrivateRoute>
-                  <AddTeamPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              exact
-              path="select"
-              element={
-                <PrivateRoute>
-                  <SelectTeamPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              exact
-              path="info"
-              element={
-                <PrivateRoute>
-                  <TeamMemberInfoPage />
-                </PrivateRoute>
-              }
-            />
-          </Route>
-          <Route exact path="/notice">
-            <Route
-              path=":idx"
-              element={
-                <PrivateRoute>
-                  <NoticeDetail />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="new"
-              element={
-                <PrivateRoute>
-                  <NoticeWrite />
-                </PrivateRoute>
-              }
-            />
-            <Route path="edit">
+    <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
+      <JoyCssVarsProvider>
+        {' '}
+        <BrowserRouter>
+          <Layout>
+            <Routes>
               <Route
-                path=":idx"
+                exact
+                path="/"
                 element={
                   <PrivateRoute>
-                    <NoticeWrite />
+                    <MainPage />
                   </PrivateRoute>
                 }
               />
-            </Route>
-          </Route>
-          <Route exact path="/place">
-            <Route
-              exact
-              path="write"
-              element={
-                <PrivateRoute>
-                  <PlaceWritePage />
-                </PrivateRoute>
-              }
-            />
-            <Route path="edit">
-              <Route
-                path=":idx"
-                element={
-                  <PrivateRoute>
-                    <PlaceWritePage />
-                  </PrivateRoute>
-                }
-              />
-            </Route>
-            <Route
-              path=":idx"
-              element={
-                <PrivateRoute>
-                  <PlaceDetailPage />
-                </PrivateRoute>
-              }
-            />
-          </Route>
-          <Route exact path="/tab" element={<RoundTab />} />
-          <Route exact path="/second" element={<SecondPage />} />
-          <Route exact path="/sign-in" element={<SignInPage />} />
-          <Route exact path="/sign-up" element={<SignUpPage />} />
-          <Route exact path="/sample">
-            <Route path="list" element={<SamplePage />} />
-            <Route path=":idx" element={<SampleDetailPage />} />
-            <Route path="write" element={<SampleWritePage />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+              <Route exact path="/team">
+                <Route
+                  exact
+                  path="add"
+                  element={
+                    <PrivateRoute>
+                      <AddTeamPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  exact
+                  path="select"
+                  element={
+                    <PrivateRoute>
+                      <SelectTeamPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  exact
+                  path="info"
+                  element={
+                    <PrivateRoute>
+                      <TeamMemberInfoPage />
+                    </PrivateRoute>
+                  }
+                />
+              </Route>
+              <Route exact path="/notice">
+                <Route
+                  path=":idx"
+                  element={
+                    <PrivateRoute>
+                      <NoticeDetail />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="new"
+                  element={
+                    <PrivateRoute>
+                      <NoticeWrite />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="edit">
+                  <Route
+                    path=":idx"
+                    element={
+                      <PrivateRoute>
+                        <NoticeWrite />
+                      </PrivateRoute>
+                    }
+                  />
+                </Route>
+              </Route>
+              <Route exact path="/place">
+                <Route
+                  exact
+                  path="write"
+                  element={
+                    <PrivateRoute>
+                      <PlaceWritePage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="edit">
+                  <Route
+                    path=":idx"
+                    element={
+                      <PrivateRoute>
+                        <PlaceWritePage />
+                      </PrivateRoute>
+                    }
+                  />
+                </Route>
+                <Route
+                  path=":idx"
+                  element={
+                    <PrivateRoute>
+                      <PlaceDetailPage />
+                    </PrivateRoute>
+                  }
+                />
+              </Route>
+              <Route exact path="/tab" element={<RoundTab />} />
+              <Route exact path="/second" element={<SecondPage />} />
+              <Route exact path="/sign-in" element={<SignInPage />} />
+              <Route exact path="/sign-up" element={<SignUpPage />} />
+              <Route exact path="/sample">
+                <Route path="list" element={<SamplePage />} />
+                <Route path=":idx" element={<SampleDetailPage />} />
+                <Route path="write" element={<SampleWritePage />} />
+              </Route>
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </JoyCssVarsProvider>
+    </MaterialCssVarsProvider>
   );
 };
 
